f8332723df344cb53ba6f479f8ef595982403c3c
Jesse Kawell <jakawell@gmail.com>
2024-10-01 22:51:33 -0400

    Remove .devcontainer (I'd rather simplify this)

--- .devcontainer/postCreate.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# This script is run in the container after it has been fully created
-# Note that is not run as the user, so anything in the .bashrc is not available here
-
-# Setup votes-server
-cd votes-server/
-npm ci
